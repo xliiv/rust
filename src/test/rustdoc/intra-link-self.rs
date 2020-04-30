f0a34ad4eedabb45b696fb48dd0ba1679bc81252
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2020-04-30 22:07:16 +0200

    remove old and useless code

--- src/test/rustdoc/intra-link-self.rs
+++ src/test/rustdoc/intra-link-self.rs
@@ -14,7 +14,6 @@
 // has foo/struct.Foo.html '//a/@href' '../foo/struct.Foo.html#method.new'
 
 //TODO: better name for bad, Bad, etc.
-//TODO: do we need ok, Ok, OK variants?
 
 pub struct MyStruct {
     // @has foo/struct.MyStruct.html '//a/@href' '../foo/struct.MyStruct.html#structfield.struct_field_bad'
@@ -77,6 +76,7 @@ impl MyStruct {
     }
 }
 
+//TODO: fix it
 impl MyTrait for MyStruct {
     // @has foo/struct.MyStruct.html '//a/@href' '../foo/struct.MyStruct.html#associatedtype.AssoTypeOk'
 
@@ -137,68 +137,27 @@ impl MyTrait for MyStruct {
 
 
 
-// OLD STUFF
+/// Use [`new`] to create a new instance.
+///
+/// [`new`]: Self::new
+pub struct Foo;
 
-///// Use [`new`] to create a new instance.
-/////
-///// [`new`]: Self::new
-//pub struct Foo;
-//
-//impl Foo {
-//    pub fn new() -> Self {
-//        unimplemented!()
-//    }
-//}
-//
-//// has foo/index.html '//a/@href' '../foo/struct.Bar.html#method.new2'
-//// has foo/struct.Bar.html '//a/@href' '../foo/struct.Bar.html#method.new2'
-//
-///// Use [`new2`] to create a new instance.
-/////
-///// [`new2`]: Self::new2
-//pub struct Bar;
-//
-//impl Bar {
-//    pub fn new2() -> Self {
-//        unimplemented!()
-//    }
-//}
-//
-//// TODO::: make it seperatly because it inteferes with item inside impl
-//// has foo/index.html '//a/@href' '../foo/struct.WithSelf.html#structfield.u8_field'
-//// /// [`u8_field`]
-//// ///
-//// /// [`u8_field`]: Self::u8_field
-//pub struct WithSelf {
-//
-//// has foo/struct.WithSelf.html '//a/@href' '../foo/struct.WithSelf.html#structfield.u8_field'
-//
-//    /// [`u8_field`]
-//    ///
-//    // TODO::: make it work
-//    /// [`u8_field`]: Self::u8_field
-//    // /// [`u8_field`]: WithSelf::u8_field
-//    pub u8_field: u8
-//}
-//
-//impl WithSelf {
-//
-//// has foo/struct.WithSelf.html '//a/@href' '../foo/struct.WithSelf.html#method.basic'
-//
-//    /// [`basic`]
-//    ///
-//    /// [`basic`]: Self::basic
-//    pub fn basic() {
-//        unimplemented!()
-//    }
-//
-//// has foo/struct.WithSelf.html '//a/@href' '../foo/struct.WithSelf.html#method.name_self'
-//
-//    /// [`name_self`]
-//    ///
-//    /// [`name_self`]: WithSelf::name_self
-//    pub fn name_self() {
-//        unimplemented!()
-//    }
-//}
+impl Foo {
+    pub fn new() -> Self {
+        unimplemented!()
+    }
+}
 
+// has foo/index.html '//a/@href' '../foo/struct.Bar.html#method.new2'
+// has foo/struct.Bar.html '//a/@href' '../foo/struct.Bar.html#method.new2'
+
+/// Use [`new2`] to create a new instance.
+///
+/// [`new2`]: Self::new2
+pub struct Bar;
+
+impl Bar {
+    pub fn new2() -> Self {
+        unimplemented!()
+    }
+}
