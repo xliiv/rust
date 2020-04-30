2ed234854682456ffefc6e21975307c22dc308e2
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2020-04-30 16:01:24 +0200

    minor change to run.test.sh

--- run.test.sh
+++ run.test.sh
@@ -2,9 +2,10 @@
 
 set -ex
 
-./x.py test --incremental --stage 1 --verbose rustdoc --test-args intra-link-self | tee run.test.log
 #./x.py test --incremental --stage 1 --verbose rustdoc --test-args intra-link-self --keep-stage 1 | tee run.test.log
+./x.py test --incremental --stage 1 --verbose rustdoc --test-args intra-link-self | tee run.test.log
+#./x.py test --incremental --stage 1 --verbose rustdoc | tee run.test.log
 
 #echo HTML
 #ls -la build/x86_64-unknown-linux-gnu/test/rustdoc/intra-link-self
-#firefox build/x86_64-unknown-linux-gnu/test/rustdoc/intra-link-self/foo
+firefox build/x86_64-unknown-linux-gnu/test/rustdoc/intra-link-self/foo
